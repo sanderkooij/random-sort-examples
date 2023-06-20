6a30958d5115180e5059ca1578c42cd74210be91
sanderkooij <sanderkooij@live.nl>
2023-06-21 01:19:36 +0200

    * Added timers

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use rand::prelude::*;
+use std::time::{Instant};
 
 fn main() {
+    let start = Instant::now();
     let vec = make_random_vec(1_000_000, 100);
-    for _ in 0..250 {
+    for _ in 0..2500 {
         let mut v = vec.clone();
         v.sort_unstable();
         //v.sort();
     }
+    let duration = start.elapsed();
+
+    println!("Time elapsed is: {:?}", duration);
 }
 
 pub fn make_random_vec(sz: usize, modulus: i64) -> Vec<i64> {
